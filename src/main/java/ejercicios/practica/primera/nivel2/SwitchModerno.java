2a913c88d9404e0d78d665866a2870cf21b1169a
Ivan Mauricio Velazquez Trejo <ivanmauriciovt@gmail.com>
2026-07-18 15:59:06 -0600

    Se hizo el ejercicio 2.2 en el cual tenias que hacer un programa que te diera los dias dependiendo del mes que te enconntraras (terminado)

--- src/main/java/ejercicios/practica/primera/nivel2/SwitchModerno.java
+++ src/main/java/ejercicios/practica/primera/nivel2/SwitchModerno.java
@@ -8,8 +8,38 @@ Restricción: debe usar la sintaxis moderna de switch (no el clásico con break)
 */
 package ejercicios.practica.primera.nivel2;
 
+import java.util.Scanner;
+
 public class SwitchModerno {
+    public String getMes(int mes) {
+        return switch (mes) {
+            case  1,3,5,7,8,10,12-> "el mes cuenta con 31 días";
+            case 4,6,9,11 -> "el mes cuenta con 30 días";
+            case  2 -> "cuenta con 28 días";
+            default -> "No entra entre los 12 meses principales!";
+        };
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("proximante");
+        Scanner sc = new Scanner(System.in);
+        SwitchModerno s = new SwitchModerno();
+        System.out.println("Días por el mes seleccionado");
+        boolean isValid = true;
+        while (isValid) {
+            System.out.println("Ingrese el valor del mes (1-12): ");
+            if (sc.hasNextInt()) {
+                int mes = sc.nextInt();
+                if (mes >= 1 && mes <= 12) {
+                    isValid = false;
+                    System.out.println(s.getMes(mes));
+                }else{
+                    System.out.println("Solo es posible ingresar un valor del 1 al 12");
+                }
+            }else{
+                sc.nextLine();
+                System.out.println("Ingresa un numero valido!");
+            }
+        }
     }
 }
