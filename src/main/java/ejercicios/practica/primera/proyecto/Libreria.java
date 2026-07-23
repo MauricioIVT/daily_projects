bbd788d6bcf25e7be944acaba2ee548511a9be04
Ivan Mauricio Velazquez Trejo <ivanmauriciovt@gmail.com>
2026-07-23 13:53:25 -0600

    Se hizo un mini-proyecto en el cual te muesatra varias cosas

---
+++ src/main/java/ejercicios/practica/primera/proyecto/Libreria.java
@@ -0,0 +1,169 @@
+package ejercicios.practica.primera.proyecto;
+
+import java.util.*;
+
+public class Libreria {
+    List<Libro> biblioteca = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    public record Libro(String title, String autor, int year, boolean availability) {}
+
+    public void setBiblioteca(Libro libro) {
+        this.biblioteca.add(libro);
+    }
+
+    public void listaLibros() {
+        biblioteca.forEach(libro -> System.out.println(this.infoLibro(libro)));
+    }
+
+    public String infoLibro(Libro libro) {
+        return "---------  Libro  ----------- " +
+                "\nTitulo: " + libro.title() +
+                "\nAutor: " + libro.autor() +
+                "\nYear: " + libro.year() +
+                "\nPrestamo: " + libro.availability() +
+                "\n-----------------------------\n";
+
+    }
+
+    public void buscarPorAutor(){
+        boolean salir = true;
+        while (salir) {
+            listaDeAutores();
+            System.out.println("Introduce el Autor: ");
+            String autor = sc.nextLine();
+            if (autor.isEmpty() || biblioteca.stream().noneMatch(libro -> libro.autor().equals(autor))) {
+                System.out.println("El Autor --- " + autor + " --- no existe");
+            }else{
+                biblioteca.stream()
+                        .filter(libro -> libro.autor().equals(autor))
+                        .forEach(libro -> System.out.println(this.infoLibro(libro)));
+                salir = false;
+            }
+        }
+    }
+
+    public void listaDeAutores(){
+        List<String> autores= biblioteca.stream().map(Libro::autor).distinct().toList();
+
+        autores.forEach(autor -> System.out.println("Autor: " + autor));
+    }
+
+
+    public void devPresLibros(){
+        System.out.println(""" 
+                \n ¿Desea tener algun prestamo del libro o devolución del mismo? 
+                p - prestamo 
+                d - devolución \n
+                """);
+        String respuesta = sc.nextLine();
+        if (respuesta.toLowerCase(Locale.ROOT).equals("p")){
+            System.out.println("¿Libro a prestar?");
+            listaLibros();
+            String prestamo = sc.nextLine();
+            libroPrestado(prestamo);
+
+        } else if (respuesta.toLowerCase(Locale.ROOT).equals("d")) {
+            System.out.println("¿Libro a devolver?");
+            listaLibros();
+            String prestamo = sc.nextLine();
+            libroDevuelto(prestamo);
+        }else {
+            System.out.println("La respuesta no fue adecuado solo existe p y d");
+        }
+    }
+
+    public void libroDevuelto(String title){
+        if(biblioteca.stream().noneMatch(libro -> libro.title().equals(title))){
+            System.out.println("El libro " + title + " no existe en la biblioteca. no se puede devolver algo que no existe");
+        } else if(biblioteca.stream().anyMatch(libro -> libro.title().equals(title) && libro.availability())) {
+            System.out.println("El libro " + title + " ya esta en la biblioteca. no se puede devolver algo que ya esta en la biblioteca");
+        }else{
+            System.out.println("El libro " + title + " ya lo puedes devolver");
+            System.out.println("El libro ya se encuentra en la biblioteca, gracias.");
+            biblioteca.stream().filter(libro -> libro.title().equals(title))
+                    .findFirst()
+                    .ifPresent(libro -> biblioteca.add(new Libro(libro.title(), libro.autor(), libro.year(), true)));
+            biblioteca.removeIf(libro -> libro.title().equals(title) && !libro.availability());
+        }
+    }
+
+    public void libroPrestado(String title){
+        if(biblioteca.stream().noneMatch(libro -> libro.title().equals(title))){
+            System.out.println("El libro " + title + " no existe en la biblioteca.");
+        } else if(biblioteca.stream().anyMatch(libro -> libro.title().equals(title) && !libro.availability())) {
+            System.out.println("El libro " + title + " esta prestado, vuelve mas tarde.");
+        }else{
+            System.out.println("El libro " + title + " se encuentra en la biblioteca.");
+            System.out.println("El libro ya se encuentra en tus manos, gracias por preferirnos!");
+            biblioteca.stream().filter(libro -> libro.title().equals(title))
+                    .findFirst()
+                    .ifPresent(libro -> biblioteca.add(new Libro(libro.title(), libro.autor(), libro.year(), false)));
+            biblioteca.removeIf(libro -> libro.title().equals(title) && libro.availability());
+        }
+    }
+
+    public void mostrarEstadisticas(){
+        int[] totalEstadisticas = {0,0};
+
+        biblioteca.forEach(libro -> {
+            if(libro.availability()){
+                totalEstadisticas[0]++;
+            }else{
+                totalEstadisticas[1]++;
+            }
+        });
+
+        System.out.println("Estadisticas: \n" +
+                "Total de libros prestados: " + totalEstadisticas[1] + " \n " +
+                "Total de libros disponibles: " + totalEstadisticas[0] + "\n");
+    }
+
+    public void menu(){
+        boolean menu = true;
+        while(menu) {
+            System.out.println("""
+                Hola bienveido a la libreria.!
+                ------------------------------
+                a - Listar libros
+                b - Buscar por Autor
+                c - Prestar o devolución de Libro
+                d - Mostrar estadisticas
+                e - Salir
+                """);
+
+            String respuesta = sc.nextLine();
+            switch (respuesta.toLowerCase()) {
+                case "a" -> this.listaLibros();
+                case "b" -> this.buscarPorAutor();
+                case "c" -> this.devPresLibros();
+                case "d" -> this.mostrarEstadisticas();
+                case "e" -> {
+                    System.out.println("Vuelva pronto.!");
+                    menu = false;
+                }
+                default ->
+                        System.out.println("Error al buscar tu selección solo es de a - d y tu elegiste " + respuesta);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Libreria libreria = new Libreria();
+        Libro libro = new Libro("Clean code", "Robert Cecil Martin", 2008, true);
+        Libro libro2 = new Libro("El psicoanalista", "John Katzenbach", 2002, true);
+        Libro libro3 = new Libro("La historia del loco", "John Katzenbach", 2004, false);
+        Libro libro4 = new Libro("Inteligencia emocional", "Daniel Goleman", 1995, true);
+        Libro libro5 = new Libro("Overlord", "Kugane Maruyama", 2012, true);
+
+        libreria.setBiblioteca(libro);
+        libreria.setBiblioteca(libro2);
+        libreria.setBiblioteca(libro3);
+        libreria.setBiblioteca(libro4);
+        libreria.setBiblioteca(libro5);
+
+        libreria.menu();
+    }
+}
